1ef9e2259175c438a63d4cf04a41272d9a782b5e
Carlos C Soto <eclipxe13@gmail.com>
2024-03-01 22:44:17 -0600

    Add test for RET 2.0 using a real CFDI
    The file was found at https://github.com/Gusi1804/CFDI-Parser/blob/19228da53ea5db6f8b213a8cac77751315cbc1b5/retenciones/1fc75b7e-cfef-5db1-bb55-aaf17f4bd70f.xml

--- tests/Features/QueryWithRealCfdiTest.php
+++ tests/Features/QueryWithRealCfdiTest.php
@@ -9,9 +9,22 @@ use PhpCfdi\SatEstadoRetenciones\Tests\TestCase;
 
 class QueryWithRealCfdiTest extends TestCase
 {
-    public function testQueryWithRealCfdi(): void
+    public function testQueryWithRetentions10MexicanReal(): void
     {
         $contents = $this->fileContents('ret10-mexican-real.xml');
+        $service = new Service();
+        $parameters = $service->makeParametersFromXml($contents);
+        $result = $service->query($parameters);
+
+        $this->assertTrue($result->getStatusDocument()->isActive());
+        $this->assertSame($parameters->getIssuerRfc(), $result->getIssuerRfc());
+        $this->assertSame($parameters->getReceiverRfc(), $result->getReceiverRfc());
+        $this->assertSame($parameters->getUuid(), $result->getUuid());
+    }
+
+    public function testQueryWithRetentions20MexicanReal(): void
+    {
+        $contents = $this->fileContents('ret20-mexican-real.xml');
         $service = new Service();
         $parameters = $service->makeParametersFromXml($contents);
         $result = $service->query($parameters);
